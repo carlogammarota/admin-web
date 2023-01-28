1512a22bd7b85945776110f1834ce4238e04c2d2
CharlyG <carlo.gammarota@gmail.com>
2023-01-28 19:53:43 -0300

    eliminar blog

--- src/components/DashboardAdmin.vue
+++ src/components/DashboardAdmin.vue
@@ -2,6 +2,7 @@
     <div>
         <div class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white dark:bg-gray-700 text-black dark:text-white">
 
+            
     <!-- Header -->
     <div class=" w-full flex items-center jus  tify-between h-14 text-white z-10 fixed">
     <div class="flex items-center justify-start md:justify-center pl-3 w-14 md:w-64 h-14 bg-blue-800 dark:bg-gray-800 border-none">
@@ -156,7 +157,106 @@
     <div class="h-full ml-14 mt-14 mb-10 md:ml-64">
 
     <!-- Statistics Cards -->
+    <div class="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
+  <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
+    <thead class="bg-gray-50">
+      <tr>
+        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Name</th>
+        <th scope="col" class="px-6 py-4 font-medium text-gray-900">State</th>
+        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Role</th>
+        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Team</th>
+        <th scope="col" class="px-6 py-4 font-medium text-gray-900"></th>
+      </tr>
+    </thead>
+    <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+      <tr class="hover:bg-gray-50" v-for="item in blogs" :key="item.id">
+        {{ item._id }}
+        <th class="flex gap-3 px-6 py-4 font-normal text-gray-900">
+          <div class="relative h-10 w-10">
+            <img
+              class="h-full w-full rounded-full object-cover object-center"
+              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <span class="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span>
+          </div>
+          <div class="text-sm">
+            <div class="font-medium text-gray-700">{{item.titulo}}</div>
+            <div class="text-gray-400">{{item.descripcion}}</div>
+          </div>
+        </th>
+        <td class="px-6 py-4">
+          <span
+            class="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600"
+          >
+            <span class="h-1.5 w-1.5 rounded-full bg-green-600"></span>
+            Active
+          </span>
+        </td>
+        <td class="px-6 py-4">Product Designer</td>
+        <td class="px-6 py-4">
+          <div class="flex gap-2">
+            <span
+              class="inline-flex items-center gap-1 rounded-full bg-blue-50 px-2 py-1 text-xs font-semibold text-blue-600"
+            >
+              Design
+            </span>
+            <span
+              class="inline-flex items-center gap-1 rounded-full bg-indigo-50 px-2 py-1 text-xs font-semibold text-indigo-600"
+            >
+              Product
+            </span>
+            <span
+              class="inline-flex items-center gap-1 rounded-full bg-violet-50 px-2 py-1 text-xs font-semibold text-violet-600"
+            >
+              Develop
+            </span>
+          </div>
+        </td>
+        <td class="px-6 py-4" >
+          <div class="flex justify-end gap-4">
+            <button @click="deleteItem(item._id)">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="h-6 w-6"
+                x-tooltip="tooltip"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                />
+              </svg>
+            </button>
+            <a x-data="{ tooltip: 'Edite' }" href="#">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="h-6 w-6"
+                x-tooltip="tooltip"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                />
+              </svg>
+            </a>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div> 
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-4 gap-4">
+        
         <div class="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
         <div class="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
             <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
@@ -735,17 +835,42 @@
     </div>
 </template>
 <script>
+const feathers = require("@/plugins/feathers.js")
 export default {
     data() {
         return {
-            
+            blogs: []
         }
     },
     methods: {
         logout(){
             // alert('logout')
             this.$emit('logout')
+        },
+        deleteItem(_id){
+            // console.log("id", id);
+            feathers.default.blog.remove(_id).then(data => {
+                this.getBlogs();
+                console.log('Eliminado', data)
+             }).catch(err => {
+                 console.log('err', err)
+             });
+        },
+        getBlogs(){
+            feathers.default.blog.find({
+                query: {
+                    $limit: 100
+                }
+            }).then(data => {
+                console.log("data", data.data);
+                this.blogs = data.data
+                console.log(this.blogs)
+            }).catch(console.log);
         }
+        
+    },
+    mounted() {
+        this.getBlogs();
     },
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,103 +12,7 @@
         how to start your project with tailwind css & Vue 3
     </h2>   -->
 <!-- {{ blogs }} -->
-<!-- <div class="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
-  <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
-    <thead class="bg-gray-50">
-      <tr>
-        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Name</th>
-        <th scope="col" class="px-6 py-4 font-medium text-gray-900">State</th>
-        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Role</th>
-        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Team</th>
-        <th scope="col" class="px-6 py-4 font-medium text-gray-900"></th>
-      </tr>
-    </thead>
-    <tbody class="divide-y divide-gray-100 border-t border-gray-100">
-      <tr class="hover:bg-gray-50" v-for="item in blogs" :key="item.id">
-        <th class="flex gap-3 px-6 py-4 font-normal text-gray-900">
-          <div class="relative h-10 w-10">
-            <img
-              class="h-full w-full rounded-full object-cover object-center"
-              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
-            <span class="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span>
-          </div>
-          <div class="text-sm">
-            <div class="font-medium text-gray-700">{{item.titulo}}</div>
-            <div class="text-gray-400">{{item.descripcion}}</div>
-          </div>
-        </th>
-        <td class="px-6 py-4">
-          <span
-            class="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600"
-          >
-            <span class="h-1.5 w-1.5 rounded-full bg-green-600"></span>
-            Active
-          </span>
-        </td>
-        <td class="px-6 py-4">Product Designer</td>
-        <td class="px-6 py-4">
-          <div class="flex gap-2">
-            <span
-              class="inline-flex items-center gap-1 rounded-full bg-blue-50 px-2 py-1 text-xs font-semibold text-blue-600"
-            >
-              Design
-            </span>
-            <span
-              class="inline-flex items-center gap-1 rounded-full bg-indigo-50 px-2 py-1 text-xs font-semibold text-indigo-600"
-            >
-              Product
-            </span>
-            <span
-              class="inline-flex items-center gap-1 rounded-full bg-violet-50 px-2 py-1 text-xs font-semibold text-violet-600"
-            >
-              Develop
-            </span>
-          </div>
-        </td>
-        <td class="px-6 py-4" >
-          <div class="flex justify-end gap-4">
-            <a x-data="{ tooltip: 'Delete' }" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="h-6 w-6"
-                x-tooltip="tooltip"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                />
-              </svg>
-            </a>
-            <a x-data="{ tooltip: 'Edite' }" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="h-6 w-6"
-                x-tooltip="tooltip"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
-                />
-              </svg>
-            </a>
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div> -->
+
 
   
 <!-- {{ token }} -->
@@ -125,7 +29,7 @@
         <div class='hidden lg:flex flex-col justify-between bg-[#ffe85c] lg:p-8 xl:p-12 lg:max-w-sm xl:max-w-lg'>
           <div class="flex items-center justify-start space-x-3">
             <span class="bg-black rounded-full w-8 h-8"></span>
-            <a href="#" class="font-medium text-xl">Brand</a>
+            <a href="#" class="font-medium text-xl">Aura Dev</a>
           </div>
           <div class='space-y-5'>
             <h1 class="lg:text-3xl xl:text-5xl xl:leading-snug font-extrabold">
@@ -164,7 +68,7 @@
             <div class="flex flex-col max-w-md space-y-5">
               <input v-model="email" type="text" placeholder="NOMBRE DE USUARIO"
                 class="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal" />
-              <input v-model="password" type="text" placeholder="CONTRASEÑA"
+              <input v-model="password" type="password" placeholder="CONTRASEÑA"
                 class="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal" />
               <button class="flex items-center justify-center flex-none px-3 py-2 md:px-4 md:py-3 border-2 rounded-lg font-medium border-black bg-black text-white" @click="login()">ENTRAR</button>
               <!-- <div class="flex justify-center items-center">
@@ -188,7 +92,7 @@
 
           <!-- Footer -->
           <div class="flex justify-center flex-col m-auto mb-16 text-center text-lg dark:text-slate-200 ">
-            <p class="font-bold mb-1">Built by <a href="#" class="underline dark:text-white">Frank Esteban</a></p>
+            <p class="font-bold mb-1">Built by <a href="#" class="underline dark:text-white">Aura Dev</a></p>
             <p>Contact me on the different platforms and social networks</p>
             <div class="flex items-center justify-center space-x-2 mt-4 flex-wrap"><a
                 href="https://www.linkedin.com/in/frankuxui/"
@@ -285,25 +189,15 @@ export default {
       password: "",
       token: null,
       // loginStatus: false,
-      blogs: []
+      blogs: [],
+      user: {}
     }
   },
   async mounted () {
-    feathers.default.blog.find({
-            
-            // query: {
-            // // read: false,
-            // // roomId: 2,
-            //     $limit: 100,
-            // }
-            query: {
-                $limit: 100
-            }
-        }).then(data => {
-            console.log("data", data.data);
-            this.blogs = data.data
-            console.log(this.blogs)
-        }).catch(console.log);
+    if (localStorage.token) {
+      this.token = localStorage.token;
+    };
+
   },
   methods: {
     login(){
@@ -322,6 +216,11 @@ export default {
         console.log('Datos Correctos')
         console.log('respuesta', response.data);
         this.token = response.data.accessToken;
+        this.user = response.data.user;
+        console.log('user', this.user);
+        
+        //Guardamos el token en localstor age para que cuando se refresque la pesta
+        localStorage.token = response.data.accessToken;
         // this.loginStatus = true;
       })
       .catch(error => {
@@ -333,6 +232,9 @@ export default {
     logout(){
       console.log('logout');
       this.token = null;
+
+      //borramos el token tambien del localStorage
+      localStorage.token = null;
     }
   },
 }
