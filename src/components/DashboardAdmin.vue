51e2a845b1d88dcf664428274c52360f26e9a34a
CharlyG <carlo.gammarota@gmail.com>
2023-01-30 18:51:10 -0300

    fix modal

--- src/components/DashboardAdmin.vue
+++ src/components/DashboardAdmin.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
             <!-- pop up Delete -->
-    <div  v-if="modalCancel" id="popup-modal" tabindex="-1" class="bg-black fixed top-0 left-0 right-0 z-50 p-4 overflow-y-auto md:inset-0 h-modal md:h-full h-screen w-full">
+    <div  v-if="modalStatus" id="popup-modal" tabindex="-1" class="bg-black fixed top-0 left-0 right-0 z-50 p-4 overflow-y-auto md:inset-0 h-modal md:h-full h-screen w-full">
         <div class="relative w-full h-full max-w-md md:h-auto m-auto mt-24">
             <div class="relative bg-gray-100 rounded-lg shadow dark:bg-gray-700">
                 <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
@@ -11,10 +11,15 @@
                 <div class="p-6 text-center">
                     <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                     <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Estas seguro que quieres eliminar este blog ?</h3>
-                    <button data-modal-hide="popup-modal" @click="DeleteItem()" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+
+                    <button data-modal-hide="popup-modal" @click="ConfirmDeleteItem()" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                         Eliminar
                     </button>
-                    <button data-modal-hide="popup-modal" v-if="modalCancel = false" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+
+                    <button data-modal-hide="popup-modal" @click="modalStatus = false" 
+                    type="button" 
+                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                    No, cancel</button>
                 </div>
             </div>
         </div>
@@ -278,7 +283,8 @@
         </td>
         <td class="px-6 py-4" >
           <div class="flex justify-end gap-4">
-            <button @click="ConfirmDeleteItem(item._id)">
+            <button @click="showModal(item._id)">
+                asd
         
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -868,32 +874,27 @@ export default {
             blogs: [],
             blogCantidad: 0,
             loader: false,
-            modalCancel: true
+            modalStatus: false,
+            delete_id: ''
+
         }
     },
     methods: {
         logout(){
-            // alert('logout')
             this.$emit('logout')
         },
-        deleteItem(_id){
-            // console.log("id", id);
-            feathers.default.blog.remove(_id).then(data => {
+        deleteItem(){
+            feathers.default.blog.remove(this.delete_id).then(data => {
                 this.getBlogs();
                 console.log('Eliminado', data)
-                // this.modalCancel = false;
+                this.hiddenModal()
              }).catch(err => {
                  console.log('err', err)
-                //  this.modalCancel = false;
+                this.hiddenModal()
              });
         },
-        ConfirmDeleteItem(_id){
-            
-            //logica de eliminado
-            // this.ConfirmDeleteItem();
-            // this.deleteItem(_id);
-            this.modalCancel = true; 
-            // alert(this.modalCancel)
+        ConfirmDeleteItem(){
+            this.deleteItem();
         },
         getBlogs(){
             this.loader = true
@@ -911,12 +912,18 @@ export default {
                 this.loader = false
                 console.log("error", error)
             });
+        },
+        showModal(_id){
+            this.delete_id = _id;
+            this.modalStatus = true;
+        },
+        hiddenModal(){
+            this.modalStatus = false;
         }
         
     },
     mounted() {
         this.getBlogs();
-        // alert(this.blogs.length)
     },
 }
 </script>
