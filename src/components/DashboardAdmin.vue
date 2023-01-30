7859957818e7ea5e542884a7945549c4e9a70be4
CharlyG <carlo.gammarota@gmail.com>
2023-01-30 19:20:01 -0300

    edit blog

--- src/components/DashboardAdmin.vue
+++ src/components/DashboardAdmin.vue
@@ -4,7 +4,7 @@
     <div  v-if="modalStatus" id="popup-modal" tabindex="-1" class="bg-black fixed top-0 left-0 right-0 z-50 p-4 overflow-y-auto md:inset-0 h-modal md:h-full h-screen w-full">
         <div class="relative w-full h-full max-w-md md:h-auto m-auto mt-24">
             <div class="relative bg-gray-100 rounded-lg shadow dark:bg-gray-700">
-                <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
+                <button type="button"  @click="hiddenModal()" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
                     <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     <span class="sr-only">Close modal</span>
                 </button>
@@ -12,15 +12,48 @@
                     <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                     <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Estas seguro que quieres eliminar este blog ?</h3>
 
-                    <button data-modal-hide="popup-modal" @click="ConfirmDeleteItem()" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                    <button data-modal-hide="popup-modal" @click="deleteItem()" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                         Eliminar
                     </button>
 
-                    <button data-modal-hide="popup-modal" @click="modalStatus = false" 
+                    <button data-modal-hide="popup-modal" @click="hiddenModal()" 
                     type="button" 
                     class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
                     No, cancel</button>
                 </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- MODAL EDITAR -->
+    <div  v-if="modalEdit" id="popup-modal" tabindex="-1" class="bg-black fixed top-0 left-0 right-0 z-50 p-4 overflow-y-auto md:inset-0 h-modal md:h-full h-screen w-full">
+        <div class="relative w-full h-full max-w-md md:h-auto m-auto mt-24">
+            <div class="relative bg-gray-100 rounded-lg shadow dark:bg-gray-700">
+                <button type="button" @click="HiddenModalEdit()" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+                
+                <div class="p-6">
+                   <h3 class="mb-5 text-lg font-normal ">Editar Blog</h3>
+                   {{ edit_id }}
+                   <br>
+                   <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white float-left">Titulo</label>
+                    <input v-model="titulo" type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white float-left mt-4">Descripcion</label>
+                    <textarea id="message" v-model="descripcion" rows="4" class="mb-4 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+
+
+                    <button  @click="guardarEdit()" type="button" class="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                        Guardar
+                    </button>
+
+                    <button data-modal-hide="popup-modal" @click="HiddenModalEdit()" 
+                    type="button" 
+                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                    Cancelar</button>
+                </div> 
             </div>
         </div>
     </div>
@@ -238,7 +271,6 @@
     <tbody class="divide-y divide-gray-100 border-t border-gray-100">
       <tr class="hover:bg-gray-50" v-for="item in blogs" :key="item.id">
         {{ item._id }}
-        {{ modalCancel }}
         <th class="flex gap-3 px-6 py-4 font-normal text-gray-900">
           <div class="relative h-10 w-10">
             <img
@@ -284,8 +316,6 @@
         <td class="px-6 py-4" >
           <div class="flex justify-end gap-4">
             <button @click="showModal(item._id)">
-                asd
-        
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -302,7 +332,7 @@
                 />
               </svg>
             </button>
-            <a x-data="{ tooltip: 'Edite' }" href="#">
+            <button @click="ShowModalEdit(item._id)">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -318,7 +348,7 @@
                   d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
                 />
               </svg>
-            </a>
+            </button>
           </div>
         </td>
       </tr>
@@ -875,7 +905,12 @@ export default {
             blogCantidad: 0,
             loader: false,
             modalStatus: false,
-            delete_id: ''
+            modalEdit: false,
+            delete_id: '',
+            edit_id: '',
+            titulo: '',
+            descripcion: ''
+
 
         }
     },
@@ -892,9 +927,6 @@ export default {
                  console.log('err', err)
                 this.hiddenModal()
              });
-        },
-        ConfirmDeleteItem(){
-            this.deleteItem();
         },
         getBlogs(){
             this.loader = true
@@ -919,6 +951,40 @@ export default {
         },
         hiddenModal(){
             this.modalStatus = false;
+        },
+        ShowModalEdit(_id){
+            this.modalEdit = true;
+            this.edit_id = _id;
+            this.getBlogId(_id);
+
+        },
+        HiddenModalEdit(){
+            this.modalEdit = false;
+
+        },
+        getBlogId(_id){
+            feathers.default.blog
+                .get(_id
+                ) 
+                .then((data) => {
+                    console.log('BLOG UNICO', data);
+                    this.titulo = data.titulo;
+                    this.descripcion = data.descripcion;
+                })
+                .catch(console.log)
+        },
+        guardarEdit(){
+            feathers.default.blog
+                .update(this.edit_id, {
+                    titulo: this.titulo,
+                    descripcion: this.descripcion
+                } ) 
+                .then((data) => {
+                    this.HiddenModalEdit();
+                    this.getBlogs();
+                    console.log('se actualizo correctamente', data)
+                })
+                .catch(console.log)
         }
         
     },
