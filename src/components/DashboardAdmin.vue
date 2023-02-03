6f1dc913f84a64d4bdfecb8fcb350343a19dfc48
CharlyG <carlo.gammarota@gmail.com>
2023-02-03 14:39:16 -0300

    asd

--- src/components/DashboardAdmin.vue
+++ src/components/DashboardAdmin.vue
@@ -228,7 +228,7 @@
                 <span
                 class="inline-flex items-center gap-1 rounded-full bg-blue-50 px-2 py-1 text-xs font-semibold text-blue-600"
                 >
-                {{item.creador.role}}
+                {{item.creador.permissions}}
                 </span>    
             </div>
             
